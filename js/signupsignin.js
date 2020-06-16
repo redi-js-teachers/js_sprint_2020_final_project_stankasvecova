8bab0f17f13cfe08881b1c0e628e7d22df58cbdb
stankasvecova <62797218+stankasvecova@users.noreply.github.com>
2020-06-16 14:23:58 +0200

    updated debugged signup-signin files

--- js/signupsignin.js
+++ js/signupsignin.js
@@ -1,14 +1,3 @@
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBbuvg-wCgz30zyD4zETU0HiHiC5_FulWc",
-  authDomain: "good-teens.firebaseapp.com",
-  databaseURL: "https://good-teens.firebaseio.com",
-  projectId: "good-teens",
-  storageBucket: "good-teens.appspot.com",
-  messagingSenderId: "260739514487",
-  appId: "1:260739514487:web:8197a36e539f090f2ec672",
-  measurementId: "G-0WBRPFKT5Z"
-};
 const signUpEl = document.getElementById("signUpBtn");
 const signInEl = document.getElementById("signInBtn");
 
@@ -124,7 +113,7 @@ firebase.auth().signInWithPopup(provider).then(function (result) {
   // ...
 });
 
-signUpEl.addEventListener(click, signUp);
-signInEl.addEventListener(click, signIn);
+signUpEl.addEventListener("click", signUp);
+signInEl.addEventListener("click", signIn);
 
 
