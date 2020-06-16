28a470fe6b7adf0be44b66ed08749be5e53bfc80
stankasvecova <62797218+stankasvecova@users.noreply.github.com>
2020-06-17 00:12:15 +0200

    updates

--- js/signupsignin.js
+++ js/signupsignin.js
@@ -41,58 +41,107 @@ firebase.auth().onAuthStateChanged(function (user) {
 });
 
 function signedInPage() {
-  document.body.innerHTML = `
+  document.body.innerHTML = `    
+  <header>
   <h1 class="regTitle"><img src="logo1.png" class="gtlogo" alt="goodteens"></h1>
-  <h1 class="regTitle"> Create your profile </h1>
-  <form name="RegForm" action="process.php" method="post" autocomplete="off">
-      <div class="row">
-          <div class="col-sm-3">
-          </div>
-          <div class="col-sm-3">
-              <label for="firstname">First name</label>
-              <input type="text" size=65 name="firstname" class="form-control" id="firstname" placeholder="albert"
-                  required>
-          </div>
-          <div class="col-sm-3">
-              <label for="city">City</label>
-              <input type="text" size=40 name="city" class="form-control" id="city" placeholder="munich" required>
-          </div>
-          <div class="col-sm-3">
-          </div>
+  <h1 class="regTitle">Your profile page</h1>
+  </header>
+  <hr>
+
+<form name="RegForm" action="process.php" method="post" autocomplete="off">
+  <div class="row">
+      <div class="col-sm-3">
       </div>
-      <div class="row">
-          <div class="col-sm-3">
-          </div>
-          <div class="col-sm-3">
-              <label for="lastname">Last name</label>
-              <input type="text" size=65 name="lastname" class="form-control" id="lastname" placeholder="einstein"
-                  required>
-          </div>
-          <div class="col-sm-3 autocomplete">
+      <div class="col-sm-3">
+          <label for="photo">Upload your photo</label>
+          <input id="image" type="file" name="profilePhoto" capture>
+      </div>
+      <div class="col-sm-3">
+          <img class="card" src="monster.jpg" alt="profile picture">
+      </div>
+      <div class="col-sm-3">
+      </div>
+  </div>
+  <br>
+  <div class="row">
+      <div class="col-sm-3">
+      </div>
+      <div class="col-sm-3">
+          <label for="firstname">First name</label>
+          <input type="text" size=65 name="firstname" class="form-control" id="firstname" placeholder="albert"
+              required>
+      </div>
+      <div class="col-sm-3">
+          <label for="city">City</label>
+          <input type="text" size=40 name="city" class="form-control" id="city" placeholder="munich" required>
+      </div>
+      <div class="col-sm-3">
+      </div>
+  </div>
+  <div class="row">
+      <div class="col-sm-3">
+      </div>
+      <div class="col-sm-3">
+          <label for="lastname">Last name</label>
+          <input type="text" size=65 name="lastname" class="form-control" id="lastname" placeholder="einstein"
+              required>
+      </div>
+      <div class="col-sm-3 autocomplete">
           <label for="validationCustom05">Country</label>
           <input class="form-control" id="country" type="text" name="country" placeholder="Country" required>
       </div>
-          <div class="col-sm-3">
-          </div>
+      <div class="col-sm-3">
+      </div>
+  </div>
+  <div class="row">
+      <div class="col-sm-3">
+      </div>
+
+      <div class="col-sm-3">
+          <label for="agecheck">Your age</label>
+          <input type="number" size=65 name="yearuser" class="form-control" id="agecheck"
+              placeholder="type number" required>
+      </div>
+      <div class="col-sm-3">
+          <label for="socialProfile">Your profile elsewhere</label>
+          <input type="text" size=65 name="socialProfile" class="form-control" id="socialProfile"
+              placeholder="your youtube/facebook/instagram">
+      </div>
+      <div class="col-sm-3"></div>
+  </div>
+  <div class="row">
+      <div class="col-sm-3">
+      </div>
+
+      <div class="col-sm-3">
+          <label for="yourWebsite">What will you be when you grow up?</label>
+          <input type="text" size=65 name="futurePlans" class="form-control" id="futurePlans"
+              placeholder="both silly dreams or good plans are fine here">  
+      </div>
+      <div class="col-sm-3">
+          <label for="yourWebsite">Your website</label>
+          <input type="text" size=65 name="yourWebsite" class="form-control" id="yourWebsite"
+              placeholder="your cool website">
+      </div>
+      <div class="col-sm-3">
+          
+      </div>
       </div>
       <div class="row">
           <div class="col-sm-3">
           </div>
-
-          <div class="col-sm-3">
-              <label for="agecheck">Your age in years</label>
-              <input type="number" size=65 name="yearuser" class="form-control" id="agecheck"
-                  placeholder="type number" required>
-          </div>
-          <div class="col-sm-3">
-                  <button class="btn" type="submit">Create an account</button></div>
-          </div>
           <div class="col-sm-3">
           </div>
-      </div>
-  </form>
-  <script src="registration.js"></script>
-  `};
+          <div class="col-sm-3">
+              <button class="btn" type="submit">Create your profile</button>
+          </div>
+          <div class="col-sm-3">   
+          </div>
+          </div>
+      
+  </div>
+</form>
+<script src="createprofile.js"></script>`};
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
