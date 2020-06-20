42f329a1b9c19377e5fad686ee2721ec5979da57
Travis Shears <t@travisshears.com>
2020-06-20 19:27:55 +0200

    create a basic working list of videos

--- js/videoBlock.js
+++ js/videoBlock.js
@@ -1,52 +1,24 @@
-const videos = [
-    {
-        title: "The youngest Beatles tribute band",
-        frameborder: 0,
-        accelerometer: true,
-        encryptedMedia: true,
-        gyroscope: true,
-        pictureInPicture: true,
-        fullscreen: true,
-        src: source[i],
-        rating: 10,
-    },
-    {
-        title: "Teen Millionaires - successful businesses run by teens",
-        frameborder: 0,
-        accelerometer: true,
-        encryptedMedia: true,
-        gyroscope: true,
-        pictureInPicture: true,
-        fullscreen: true,
-        src: source[i],
-        rating: 20,
-    },
+const ytVideoIDs = [
+    'mRmMGeyruB0',
+    'Unx9Y_Wy5pI',
+    'sDnNpL2eF98'
 ]
 
-let source = ["https://www.youtube.com/embed/Uq-FOOQ1TpE", "https://www.youtube.com/embed/93mctUlDofw", "https://www.youtube.com/embed/1YCGM7FiCGg", "https://www.youtube.com/embed/vPukHebsQeQ", "https://www.youtube.com/embed/h11u3vtcpaY"]
-
-function displayVideos(videos) {
-    for (let i = 0; i < video.length; i++) {
-        let videosEl = document.getElementById("video");
-        videos.src = source[i].value;
-        let videosEl = document.createElement("div");
-        source.setAttribute('src', source[i]);
-        videosEl.appendChild(source);
-        videos.play();
-        videos.innerHTML = `
-      <p> <h3><b>${videos.title}</b></h3></p>
-      <p> ${rateVideos(videos.rating)} </p>
-      `
-    }
+function videoEl(videoId) {
+    const videoWrapperEl = document.createElement('div');
+    videoWrapperEl.innerHTML = `
+    <iframe
+        type="text/html"
+        width="640"
+        height="360"
+        src="https://www.youtube.com/embed/${videoId}?autoplay=0&origin=http://localhost"
+        frameborder="0"
+    ></iframe>`;
+    return videoWrapperEl;
 }
 
-displayVideos(videos);
+const videoListEl = document.querySelector('.gridVideo');
 
-function rateVideos(rating) {
-    const medalEl = document.getElementById("medal"),
-    const pooEl = document.getElementById("poo"),
-
-};
-
-
-
+ytVideoIDs.forEach(videoId => {
+    videoListEl.appendChild(videoEl(videoId));
+})
